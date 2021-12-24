bc49f89856d8725325a1f70d869c04ab6b6fb665
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2021-12-24 23:45:44 +0530

    update

--- react-intro/react-intro/src/App.js
+++ react-intro/react-intro/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const OS = ["Andorid", "BlackBarry", "iphone", "Window Phone"];
+  const brand = ["Samsung", "HTC", "Micromax", "Apple"];
   return (
-    <h1>Welcome</h1>
+    <div className="App">
+      <h1 className="heading">Mobile Operating System</h1>
+      <ul>
+        {OS.map((e) => (
+          <Lists op={e} />
+        ))}
+      </ul>
+      ;<h1 className="heading">Mobile Manufatures</h1>
+      <ul>
+        {brand.map((e) => (
+          <Lists op={e} />
+        ))}
+      </ul>
+    </div>
   );
+}
+function Lists({ op }) {
+  return <li> {op} </li>;
 }
 
 export default App;
