eece643de25c5f06d6160b21e030cde067c8bc26
Smrutiranjan Patra <76831153+Smrutiranjan-Patra@users.noreply.github.com>
2021-12-25 00:17:09 +0530

    Complete

--- react-stateandprop/myapp/src/App.js
+++ react-stateandprop/myapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const multi = () => {
+    setCount(count * 2);
+  };
+
+  const changeCount = (value) => {
+    setCount(count + value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="counter">Counter : {count}</h1>
+      <button onClick={() => changeCount(1)}>Increase</button>
+      <button onClick={() => changeCount(-1)}>Decrease</button>
+      <button onClick={multi}>Double</button>
     </div>
   );
 }
